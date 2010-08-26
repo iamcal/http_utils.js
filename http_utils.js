0136d32459c8854a95a75603a09d256f940cbc8e
Cal Henderson <cal@iamcal.com>
2010-08-26 16:10:28 -0500

    moved http_buffered calss into http_utils and renamed it. fixed an issue with body stream being appended to null

--- http_utils.js
+++ http_utils.js
@@ -1,4 +1,10 @@
+var http = require('http');
+var url = require('url');
+var querystring = require('querystring');
+
 var http_subparse = require('./http_subparse');
+
+var http_utils = exports;
 
 //
 // tokens used in the HTTP spec
@@ -104,3 +110,85 @@ exports.parse_multipart = function(bound, body){
 
 	return parts;
 }
+
+
+//
+// a simple subclass of http.createServer which doesn't fire the callback until
+// the request body has been buffered, but only if we care about its contents.
+// we also parse out get and post args.
+//
+
+exports.createSimpleServer = function(callback){
+
+	return http.createServer(function (req, res) {
+
+		req.body = null;
+		req.setEncoding(encoding='utf8');
+
+		var _url = url.parse(req.url, true);
+		req.get = _url.query || {};
+		req.post = {};
+
+
+		//
+		// do we want to wait for the body?
+		//
+
+		var wait_for_body = 0;
+
+		var content_type = http_utils.parse_header('content-type', req.headers['content-type']);
+
+		if (req.method == 'POST'){
+			if (content_type.fulltype == 'application/x-www-form-urlencoded'){
+
+				wait_for_body = 'url';
+			}
+
+			if (content_type.fulltype == 'multipart/form-data'){
+
+				wait_for_body = 'multi';
+				req.body_bound = content_type.boundary;
+			}
+		}
+
+
+		//
+		// get on with it
+		//
+
+		if (wait_for_body){
+
+			req.on('data', function(chunk){
+				if (req.body){
+					req.body += chunk;
+				}else{
+					req.body = chunk;
+				}
+			});
+
+			req.on('end', function(){
+
+				if (wait_for_body == 'url'){
+					//console.log('parsing url encoded body args');
+					req.post = querystring.parse(req.body);
+				}
+
+				if (wait_for_body == 'multi'){
+
+					// TODO: this should stream the request to the multipart pasrer, rather
+					// than buffer it all up first.
+					req.parts = http_utils.parse_multipart(content_type.boundary, req.body);
+
+					//console.log('MULTIPART!');
+					//console.log(content_type.boundary);
+					//console.log(req.body);
+				}
+
+				callback(req, res);
+			});
+		}else{
+
+			callback(req, res);
+		}
+	});
+};
